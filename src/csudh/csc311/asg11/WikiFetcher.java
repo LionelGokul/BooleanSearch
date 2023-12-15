124cf47ad53c8ac3e38d077c82a07e47aaa532f6
sgokul-cavintek <145082356+Gokul-sekar@users.noreply.github.com>
2023-12-15 10:26:30 -0800

    initial code push

---
+++ src/csudh/csc311/asg11/WikiFetcher.java
@@ -0,0 +1,89 @@
+package csudh.csc311.asg11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WikiFetcher {
+	private long lastRequestTime = -1;
+	private long minInterval = 1000;
+
+	/**
+	 * Fetches and parses a URL string, returning a list of paragraph elements.
+	 *
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Elements fetchWikipedia(String url) throws IOException {
+		sleepIfNeeded();
+
+		// download and parse the document
+		Connection conn = Jsoup.connect(url);
+		Document doc = conn.get();
+
+		// select the content text and pull out the paragraphs.
+		Element content = doc.getElementById("mw-content-text");
+
+		Elements paras = content.select("p");
+		return paras;
+	}
+
+	/**
+	 * Reads the contents of a Wikipedia page from src/resources.
+	 *
+	 * @param url
+	 * @return
+	 * @throws IOException
+	 */
+	public Elements readWikipedia(String url) throws IOException {
+		// Connect to the URL and fetch the document
+		Document doc = Jsoup.connect(url).get();
+
+		// Select the content text and extract paragraphs
+		Element content = doc.getElementById("mw-content-text");
+		Elements paras = content.select("p");
+		return paras;
+	}
+
+	/**
+	 * Rate limits by waiting at least the minimum interval between requests.
+	 */
+	private void sleepIfNeeded() {
+		if (lastRequestTime != -1) {
+			long currentTime = System.currentTimeMillis();
+			long nextRequestTime = lastRequestTime + minInterval;
+			if (currentTime < nextRequestTime) {
+				try {
+					// System.out.println("Sleeping until " + nextRequestTime);
+					Thread.sleep(nextRequestTime - currentTime);
+				} catch (InterruptedException e) {
+					System.err.println("Warning: sleep interrupted in fetchWikipedia.");
+				}
+			}
+		}
+		lastRequestTime = System.currentTimeMillis();
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		WikiFetcher wf = new WikiFetcher();
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		Elements paragraphs = wf.fetchWikipedia(url);
+
+		for (Element paragraph : paragraphs) {
+			System.out.println(paragraph);
+		}
+	}
+}
